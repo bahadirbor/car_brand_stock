182a3eb8546ec783f7d2f3837577eab9c1f2ca05
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-08-11 18:11:03 +0300

    Fixed column issues

--- data/automotive.sql
+++ data/automotive.sql
@@ -1,8 +1,8 @@
-CREATE TABLE NOT EXISTS Stocks(
+CREATE TABLE IF NOT EXISTS Stocks(
     date_time TEXT NOT NULL,
     stock_name TEXT NOT NULL,
     open_value REAL NOT NULL,
-    open_value REAL NOT NULL,
+    high_value REAL NOT NULL,
     low_value REAL NOT NULL,
     close_value REAL NOT NULL,
     daily_return REAL NOT NULL,
