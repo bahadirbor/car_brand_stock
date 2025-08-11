9bdf9976053f4317bd867f312225afac9157ee40
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-08-11 17:54:15 +0300

    Created database structure

---
+++ data/automotive.sql
@@ -0,0 +1,11 @@
+CREATE TABLE NOT EXISTS Stocks(
+    date_time TEXT NOT NULL,
+    stock_name TEXT NOT NULL,
+    open_value REAL NOT NULL,
+    open_value REAL NOT NULL,
+    low_value REAL NOT NULL,
+    close_value REAL NOT NULL,
+    daily_return REAL NOT NULL,
+    adj_close REAL NOT NULL,
+    volume INTEGER NOT NULL
+);
